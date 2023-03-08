a42af87933668f3b9df2f418eee27255fe5e8650
Kyle Koser <kkoser@kkoser-mn3.linkedin.biz>
2023-03-08 11:20:25 -0800

    Delete a dependency, which should trigger a re-generation of the dagger graph but does not, causing compilation errors

--- MyLibrary/src/main/java/com/example/mylibrary/MyBiggerDependency.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mylibrary
-
-import javax.inject.Inject
-
-class MyBiggerDependency @Inject constructor(private val myDependency: MyDependency) {
-}
--- app/src/main/java/com/example/daggerfactoryexample/MyTransformer.java
+++ app/src/main/java/com/example/daggerfactoryexample/MyTransformer.java
@@ -2,14 +2,12 @@ package com.example.daggerfactoryexample;
 
 import androidx.annotation.NonNull;
 
-import com.example.mylibrary.MyBiggerDependency;
-import com.example.mylibrary.MyDependency;
 
 import javax.inject.Inject;
 
 public class MyTransformer {
     @Inject
-    public MyTransformer(@NonNull MyBiggerDependency myBiggerDependency) {
+    public MyTransformer() {
 
     }
 }
